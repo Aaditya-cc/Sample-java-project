e297b9495df64652aa450de31c41072aed1e0812
Aaditya Nandeshwar <aaditya.nandeshwar@cldcvr.com>
2022-07-14 16:33:12 +0530

    Added java demo application files

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //code will go in here
+        System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
+    }
+
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
+
